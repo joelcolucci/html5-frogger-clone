d84005abe0758511e66861f9afc8adfcdbe70ab4
JoelColucci <JoelColucci@Gmail.com>
2014-11-30 20:13:30 -0500

    Added JSON handler that serves top 5 in json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import webapp2
 import jinja2
+import json
 
 from google.appengine.ext import db
 
@@ -21,7 +22,7 @@ class MainPage(webapp2.RequestHandler):
     #Get the parameters from the request
     initials = self.request.get('initials')
     location = self.request.get('location')
-    score = self.request.get('score')
+    score = int(self.request.get('score'))
 
     #TODO: Validate form post
 
@@ -32,12 +33,32 @@ class MainPage(webapp2.RequestHandler):
 
 
 
+class JsonHandler(webapp2.RequestHandler):
+  #SOURCE: Udacity Course: Web Dev - Lesson 5
+  def get(self):
+    # get JSON
+
+    q = HighScore.all()
+    db.delete(q)
+    highscores = q.order('-score').fetch(limit=5)
+
+    # serve JSON
+    return self.render_json([s.as_dict() for s in highscores])
+
+  #SOURCE: Udacity Course: Web Dev - Lesson 5
+  def render_json(self, d):
+    json_txt = json.dumps(d)
+    self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+    self.response.write(json_txt)
+
+
+
 ### Data Models ###
 class HighScore(db.Model):
   """datastore data model for HighScore"""
   initials = db.StringProperty(required = True)
   location = db.StringProperty(required = True)
-  score = db.StringProperty(required = True)
+  score = db.IntegerProperty(required = True)
 
   @classmethod
   def newScore(cls, initials, location, score):
@@ -53,10 +74,19 @@ class HighScore(db.Model):
     highscores = q.order('-score').fetch(limit=5)
     return highscores
 
+  #SOURCE: Udacity Course: Web Dev - Lesson 5
+  def as_dict(self):
+    d = {'initials': self.initials,
+         'location': self.location,
+         'score': self.score
+    }
+    return d
+
 ### Utilities ###
 
 
 
 application = webapp2.WSGIApplication([
   ('/', MainPage),
+  ('/json', JsonHandler)
 ], debug=True)
